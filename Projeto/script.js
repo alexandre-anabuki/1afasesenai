af88daf5b31931fd30d92c95b396f5ed3542df0f
alexandre-anabuki <alex.anabuki@gmail.com>
2024-11-21 22:05:52 -0300

    Add files via upload

--- Projeto/script.js
+++ Projeto/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const form = document.getElementById('cadForm')
+    const form = document.getElementById('cadModal')
     form.addEventListener('submit', function(event){
         event.preventDefault()
         const ferramenta = document.getElementById('tool').value
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
         while(itens.some(item => item.id === id)) {
             id++
         }
+        // let id = Date.now()
         const item ={id, ferramenta, quantia }
         itens.push(item)
         localStorage.setItem('itens', JSON.stringify(itens))
@@ -18,7 +19,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 })
 
 let modal = document.getElementById("cadModal")
-let span = document.getElementById("close")[0]
+let span = document.getElementsByClassName("close")[0]
 span.onclick = function(){
     modal.style.display = "none"
 }
@@ -26,4 +27,31 @@ window.onclick = function(event){
     if(event.target == modal){
         modal.style.display = "none"
     }
+}
+
+function esconder(){
+    document.getElementById("cadModal").style.display = "none"
+    document.getElementById("empModal").style.display = "none"
+    document.getElementById("devModal").style.display = "none"
+    // document.getElementById("lisModal").style.display = "none"
+}
+
+function mostrarcad(){
+    esconder()
+    document.getElementById("cadModal").style.display = "block"
+}
+
+function mostraremp(){
+    esconder()
+    document.getElementById("empModal").style.display = "block"
+}
+
+function mostrardev(){
+    esconder()
+    document.getElementById("devModal").style.display = "block"
+}
+
+function mostrarlis(){
+    esconder()
+    document.getElementById("lisModal").style.display = "block"
 }
